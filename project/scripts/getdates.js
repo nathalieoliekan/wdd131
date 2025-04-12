b09d9ff4ff644163c509efca9770f8bf407c8af7
nathalieoliekan <nathalie.oliekan@gmail.com>
2025-04-12 15:22:43 -0700

    updates to shop with product javascript

--- project/scripts/getdates.js
+++ project/scripts/getdates.js
@@ -3,3 +3,18 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Get the last modified date of the document
 document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
+
+    menuButton.addEventListener("click", () => {
+        if (navMenu.style.display === "block") {
+            navMenu.style.display = "none";
+            menuButton.textContent = "☰";
+        } else {
+            navMenu.style.display = "block";
+            menuButton.textContent = "✖";
+        }
+    });
+});
